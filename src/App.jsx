4ea14378ef6bd026d24d8ba59af73cc3891a3f11
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-04-18 20:06:58 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import AboutSection from "./components/AboutSection";
 
 
 
@@ -11,6 +12,7 @@ const App = () => {
     <>
     <Header />
     <HeroSection></HeroSection>
+    <AboutSection/>
     </>
     
       
---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function AboutSection() {
+    return (
+      <section id="about" className="py-16 bg-white text-center">
+        <h2 className="text-3xl font-semibold text-blue-700">About Us</h2>
+        <p className="mt-4 max-w-2xl mx-auto text-gray-700">
+          Aarti Fabrication is known for its precision, quality, and customer satisfaction. With years of experience in the industry, we have built a strong reputation.
+        </p>
+      </section>
+    );
+  }
