fcd5b7855d864d0fcbda3459d0698156416337ac
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-05-07 18:54:31 +0530

    new

--- src/components/CallNowSection.jsx
+++ src/components/CallNowSection.jsx
@@ -4,7 +4,7 @@ export default function CallNowSection() {
   return (
     <section id="contact" className="bg-white py-10 text-center relative z-20">
       <h3 className="text-2xl md:text-3xl font-extrabold text-orange-600">
-        CALL NOW - 9594597732
+        CALL NOW - 9594597732/7021508692
       </h3>
       <p className="mt-2 text-lg font-semibold text-gray-800">
         To get affordable On-Site Fabrication Services in Maharastra
